07f8707a55572120b0b04ec24367a51747fbf277
James <qjameso@berkeley.edu>
2022-03-19 09:39:48 -0700

    completed cookieParser

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,16 @@
 const parseCookies = (req, res, next) => {
+  var cookies = {};
+  if (req.headers.cookie) {
+    var cookieArr = req.headers.cookie.split('; ');
+
+    cookieArr.forEach(c => {
+      var cookie = c.split('=');
+      cookies[cookie[0]] = cookie[1];
+    });
+  }
+
+  req.cookies = cookies;
+  next();
 };
 
 module.exports = parseCookies;
